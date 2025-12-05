659e347cb12fdc9455ab6cf05e17cab4611223b0
Eva-Contreras <evacontreras425@gmail.com>
2025-12-05 07:20:36 -0600

    05-12

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.get('/alumnos', (req, res) => {
     function (err, results, fields) {
       console.log(results); // results contains rows returned by server
       console.log(fields); // fields contains extra meta data about results, if available
+      res.json(results);
     }
   );
 })
